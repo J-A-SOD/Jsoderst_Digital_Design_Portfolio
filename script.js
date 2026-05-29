027314875ab93e0ae8b9176ba5382365aee498e3
J.A.S <jas_archi@outlook.com>
2026-05-29 15:03:28 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggledarkmode() {
+  const body = document.body;
+
+  if (body.classList.contains('dark')) {
+    body.classList.remove('dark');
+    body.classList.add('light');
+  } else {
+    body.classList.remove('light');
+    body.classList.add('dark');
+  }
+}
