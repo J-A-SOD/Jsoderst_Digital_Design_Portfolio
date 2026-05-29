70e543b7b40184e9c26804e2b4dcd0f5c56e7b04
J.A.S <jas_archi@outlook.com>
2026-05-29 15:36:29 -0700

    adding bg animation

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ function toggledarkmode() {
     body.classList.add('dark');
   }
 }
+
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = (e.clientX / window.innerWidth) - 0.5;
+  mouseY = (e.clientY / window.innerHeight) - 0.5;
+});
+
+let currentX = 0;
+let currentY = 0;
+
+function animate() {
+  // smooth interpolation (lerp)
+  currentX += (mouseX * 100 - currentX) * 1;
+  currentY += (mouseY * 100 - currentY) * 1;
+
+  bg.style.transform = `translate(${currentX}px, ${currentY}px)`;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
